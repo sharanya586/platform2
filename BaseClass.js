76a75f57205a877c0defffbf5abd23ac71c8a27e
sharanya586 <sharanyapodduturi@gmail.com>
2020-12-17 17:33:51 +0530

    added a platform

--- BaseClass.js
+++ BaseClass.js
@@ -1,11 +1,12 @@
 class BaseClass{
-    constructor(x, y, width, height, angle) {
+    constructor(x, y, width, height) {
         var options = {
             'restitution':0.8,
             'friction':1.0,
             'density':1.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
+        console.log(this.body)
         this.width = width;
         this.height = height;
         this.image = loadImage("sprites/base.png");
